40c8bc5d7aecec0d48b713250e6f43531161fc95
B-less <140559094+B-less@users.noreply.github.com>
2025-03-23 07:32:31 +0000

    Import os in wsgi and other necessary files

--- radio_site/settings.py
+++ radio_site/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
